f32dc4c2000b5b32f26448e82060610c31e6f7a9
Edwin Mo <edwin.mo@live.com>
2014-05-11 17:23:33 -0700

    Added a testDatabase class

---
+++ Selfie/app/src/main/java/tests/testDatabase.java
@@ -0,0 +1,14 @@
+package tests;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by edwinmo on 5/11/14.
+ */
+public class testDatabase extends InstrumentationTestCase {
+    /* Tests creation of a CategoryDataSource and ItemDataSource
+     *
+     */
+    public void testCreate() throws Exception{
+    }
+}
